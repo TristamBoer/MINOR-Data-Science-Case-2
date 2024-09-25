fbeb93905b04233521fedebe7d76d2ae5cf8b071
TristamBoer <127968977+TristamBoer@users.noreply.github.com>
2024-09-25 22:32:52 +0200

    Update 1_Historical_Data.py

--- pages/1_Historical_Data.py
+++ pages/1_Historical_Data.py
@@ -77,8 +77,30 @@ with col1:
 	st.markdown(
 		'''
   De DataFrame bevat de volgende kolommen:
-  - Item 1
-  - Item 2
+  - Date:
+  Bevat de datums van alle datapunten. Elk rij in dit kolom is een nieuwe dag.  
+  - temperature_2m_mean:
+  Bevat de gemiddelde temperatuur, in graden Celsius, dat op twee meter hoog is gemeten.
+  - apparant_temperature_mean:
+  Bevat de gemiddelde gevoels temperatuur, in graden Celcius, dat op twee meter is gemeten.
+  - daylight_duration:
+  Bevat hoelang, in uren, het op een dag daglicht is.
+  - sunshine_duration:
+  Bevat hoelang, in uren, de zon op een dag schijnt.
+  - precipitation_sum:
+  Bevat de som, in millimeter,  van de hoeveelheid gevallen neerslag.
+  - rain sum:
+  Bevat de som, in millimeter,  van de hoeveelheid gevallen regen.
+  - snowfall_sum:
+  Bevat de som, in millimeter,  van de hoeveelheid gevallen sneeuw.
+  - precipitation_hours:
+  Bevat hoelang, in uren, het regent op een dag.
+  - wind_speed_10m_max:
+  Bevat, in km/u,  de maximale wind snelheid op een dag.
+  - wind_gusts_10m_max:
+  Bevat, in km/u,  de maximale windvlaag snelheid op een dag.
+  - wind_direction_10m_dominant:
+  Bevat, in graden, de dominante wind directie.
   		'''
 	)
 

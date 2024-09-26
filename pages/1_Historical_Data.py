56c04c2fd2e10cd2f7ef0f7daf99350f075bcc49
TristamBoer <127968977+TristamBoer@users.noreply.github.com>
2024-09-26 10:21:17 +0200

    Update 1_Historical_Data.py

--- pages/1_Historical_Data.py
+++ pages/1_Historical_Data.py
@@ -15,65 +15,71 @@ st.set_page_config(page_title='Historical Weather Data')
 
 st.markdown('# Historical Weather Data')
 
-# Setup the Open-Meteo API client with cache and retry on error
-cache_session = requests_cache.CachedSession('.cache', expire_after = -1)
-retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
-openmeteo = openmeteo_requests.Client(session = retry_session)
+@st.cache_data
+def data():
+	# Setup the Open-Meteo API client with cache and retry on error
+	cache_session = requests_cache.CachedSession('.cache', expire_after = -1)
+	retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+	openmeteo = openmeteo_requests.Client(session = retry_session)
+	
+	# Make sure all required weather variables are listed here
+	# The order of variables in hourly or daily is important to assign them correctly below
+	url = "https://archive-api.open-meteo.com/v1/archive"
+	params = {
+		"latitude": 52.374,
+		"longitude": 4.8897,
+		"start_date": "2000-01-01",
+		"end_date": "2024-01-01",
+		"daily": ["temperature_2m_mean", "apparent_temperature_mean", "daylight_duration", "sunshine_duration", "precipitation_sum", "rain_sum", "snowfall_sum", "precipitation_hours", "wind_speed_10m_max", "wind_gusts_10m_max", "wind_direction_10m_dominant"],
+		"timezone": "Europe/Berlin"
+	}
+	responses = openmeteo.weather_api(url, params=params)
+	
+	# Process first location. Add a for-loop for multiple locations or weather models
+	response = responses[0]
+	
+	# Process daily data. The order of variables needs to be the same as requested.
+	daily = response.Daily()
+	daily_temperature_2m_mean = daily.Variables(0).ValuesAsNumpy()
+	daily_apparent_temperature_mean = daily.Variables(1).ValuesAsNumpy()
+	daily_daylight_duration = daily.Variables(2).ValuesAsNumpy()
+	daily_sunshine_duration = daily.Variables(3).ValuesAsNumpy()
+	daily_precipitation_sum = daily.Variables(4).ValuesAsNumpy()
+	daily_rain_sum = daily.Variables(5).ValuesAsNumpy()
+	daily_snowfall_sum = daily.Variables(6).ValuesAsNumpy()
+	daily_precipitation_hours = daily.Variables(7).ValuesAsNumpy()
+	daily_wind_speed_10m_max = daily.Variables(8).ValuesAsNumpy()
+	daily_wind_gusts_10m_max = daily.Variables(9).ValuesAsNumpy()
+	daily_wind_direction_10m_dominant = daily.Variables(10).ValuesAsNumpy()
+	
+	daily_data = {"date": pd.date_range(
+		start = pd.to_datetime(daily.Time(), unit = "s", utc = True),
+		end = pd.to_datetime(daily.TimeEnd(), unit = "s", utc = True),
+		freq = pd.Timedelta(seconds = daily.Interval()),
+		inclusive = "left"
+	)}
+	
+	daily_data["temperature_2m_mean"] = daily_temperature_2m_mean
+	daily_data["apparent_temperature_mean"] = daily_apparent_temperature_mean
+	daily_data["daylight_duration"] = daily_daylight_duration
+	daily_data["sunshine_duration"] = daily_sunshine_duration
+	daily_data["precipitation_sum"] = daily_precipitation_sum
+	daily_data["rain_sum"] = daily_rain_sum
+	daily_data["snowfall_sum"] = daily_snowfall_sum
+	daily_data["precipitation_hours"] = daily_precipitation_hours
+	daily_data["wind_speed_10m_max"] = daily_wind_speed_10m_max
+	daily_data["wind_gusts_10m_max"] = daily_wind_gusts_10m_max
+	daily_data["wind_direction_10m_dominant"] = daily_wind_direction_10m_dominant
+	
+	daily_dataframe = pd.DataFrame(data=daily_data)
+	daily_dataframe['date'] = daily_dataframe['date'].dt.date
+	daily_dataframe.index = daily_dataframe['date']
+	daily_dataframe['date'] = pd.to_datetime(daily_dataframe['date'], yearfirst=True)
+	daily_dataframe['daylight_duration'] = (daily_dataframe['daylight_duration'] / 60) / 60
+	daily_dataframe['sunshine_duration'] = (daily_dataframe['sunshine_duration'] / 60) / 60
+	return daily_dataframe
 
-# Make sure all required weather variables are listed here
-# The order of variables in hourly or daily is important to assign them correctly below
-url = "https://archive-api.open-meteo.com/v1/archive"
-params = {
-	"latitude": 52.374,
-	"longitude": 4.8897,
-	"start_date": "2000-01-01",
-	"end_date": "2024-01-01",
-	"daily": ["temperature_2m_mean", "apparent_temperature_mean", "daylight_duration", "sunshine_duration", "precipitation_sum", "rain_sum", "snowfall_sum", "precipitation_hours", "wind_speed_10m_max", "wind_gusts_10m_max", "wind_direction_10m_dominant"],
-	"timezone": "Europe/Berlin"
-}
-responses = openmeteo.weather_api(url, params=params)
-
-# Process first location. Add a for-loop for multiple locations or weather models
-response = responses[0]
-
-# Process daily data. The order of variables needs to be the same as requested.
-daily = response.Daily()
-daily_temperature_2m_mean = daily.Variables(0).ValuesAsNumpy()
-daily_apparent_temperature_mean = daily.Variables(1).ValuesAsNumpy()
-daily_daylight_duration = daily.Variables(2).ValuesAsNumpy()
-daily_sunshine_duration = daily.Variables(3).ValuesAsNumpy()
-daily_precipitation_sum = daily.Variables(4).ValuesAsNumpy()
-daily_rain_sum = daily.Variables(5).ValuesAsNumpy()
-daily_snowfall_sum = daily.Variables(6).ValuesAsNumpy()
-daily_precipitation_hours = daily.Variables(7).ValuesAsNumpy()
-daily_wind_speed_10m_max = daily.Variables(8).ValuesAsNumpy()
-daily_wind_gusts_10m_max = daily.Variables(9).ValuesAsNumpy()
-daily_wind_direction_10m_dominant = daily.Variables(10).ValuesAsNumpy()
-
-daily_data = {"date": pd.date_range(
-	start = pd.to_datetime(daily.Time(), unit = "s", utc = True),
-	end = pd.to_datetime(daily.TimeEnd(), unit = "s", utc = True),
-	freq = pd.Timedelta(seconds = daily.Interval()),
-	inclusive = "left"
-)}
-daily_data["temperature_2m_mean"] = daily_temperature_2m_mean
-daily_data["apparent_temperature_mean"] = daily_apparent_temperature_mean
-daily_data["daylight_duration"] = daily_daylight_duration
-daily_data["sunshine_duration"] = daily_sunshine_duration
-daily_data["precipitation_sum"] = daily_precipitation_sum
-daily_data["rain_sum"] = daily_rain_sum
-daily_data["snowfall_sum"] = daily_snowfall_sum
-daily_data["precipitation_hours"] = daily_precipitation_hours
-daily_data["wind_speed_10m_max"] = daily_wind_speed_10m_max
-daily_data["wind_gusts_10m_max"] = daily_wind_gusts_10m_max
-daily_data["wind_direction_10m_dominant"] = daily_wind_direction_10m_dominant
-
-daily_dataframe = pd.DataFrame(data=daily_data)
-daily_dataframe['date'] = daily_dataframe['date'].dt.date
-daily_dataframe.index = daily_dataframe['date']
-daily_dataframe['date'] = pd.to_datetime(daily_dataframe['date'], yearfirst=True)
-daily_dataframe['daylight_duration'] = (daily_dataframe['daylight_duration'] / 60) / 60
-daily_dataframe['sunshine_duration'] = (daily_dataframe['sunshine_duration'] / 60) / 60
+daily_dataframe = data()
 
 st.markdown(
 	'''
@@ -214,7 +220,6 @@ predictions = model.predict(forecast)
 
 fig = go.Figure()
 
-# Plot the actual data
 fig.add_trace(go.Scatter(x=forecast_data['ds'], y=forecast_data['y'],
                          mode='markers', name='Actual Data',
                          line=dict(color='blue')))
@@ -235,23 +240,18 @@ fig.add_trace(go.Scatter(
     fill='tonexty', name='Confidence Interval',
     line=dict(width=0), fillcolor='rgba(0,100,80,0.2)', showlegend=True))
 
-# Streamlit slider for date range selection
 min_date = pd.to_datetime(predictions['ds']).min().date()
 max_date = pd.to_datetime(predictions['ds']).max().date()
 
-# Create a date slider in Streamlit
-start_date, end_date = st.slider('Select date range:',
+start_date, end_date = st.slider('Select lengte datum:',
                                  min_value=min_date, max_value=max_date,
                                  value=(min_date, max_date), format="YYYY-MM-DD")
 
-# Update layout and axis ranges
 fig.update_layout(title='Temperature Forecast',
                   xaxis_title='Date',
                   yaxis_title='Temperature (°C)',
                   title_font_size=18)
 
-# Filter the data based on slider date selection
 fig.update_xaxes(range=[str(start_date), str(end_date)])
 
-# Display the plot with the slider filter
 st.plotly_chart(fig)

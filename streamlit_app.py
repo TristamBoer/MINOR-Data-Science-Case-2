a970731c5a755eb378959b83bd378aebc73a1051
TristamBoer <127968977+TristamBoer@users.noreply.github.com>
2024-09-26 11:43:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,10 @@
 import streamlit as st
 
-def wide_space_default():
-        st.set_page_config(layout='wide')
-wide_space_default()
+def page_config():
+        st.set_page_config(layout='wide',
+                          page_title="Openmeteo Weather App")
+page_config()
 
-
-st.set_page_config(
-    page_title="Openmeteo Weather App",
-)
 
 st.sidebar.success("Select a demo above.")
 

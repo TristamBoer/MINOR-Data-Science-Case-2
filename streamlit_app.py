cd094bd1d52d4ba2458e2a3ec1026cddb18f9387
TristamBoer <127968977+TristamBoer@users.noreply.github.com>
2024-09-26 12:21:12 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,8 @@ st.write(
         Voor dit dashboard is gebruikt gemaakt van weerdata afkomstig van de OpenMeteo API. OpenMeteo bevat accurate realtime en historische weerdata. 
         De data van de API is gemakkkelijk te gebruiken, doordat een overzichtelijk webpagina bevat. Waarin de user kan kiezen welke variabelen ze willen gebruiken. 
         Daarnaast is de API volledig openbaar en vereist geen API-sleutel. De data is beschikbaar is in verschillende tijdsintervallen, zoals uur- en dagvoorspellingen.
+
+        -  **https://open-meteo.com/**
         '''
 )
 
@@ -24,6 +26,8 @@ st.write(
         Daarnaast biedt de KNMI (Koninklijk Nederlands Meteorologisch Instituut) uitgebreide weer een klimaatdata voor Nederland. 
         Het KNMI, net zoals de OpenMeteo API, levert zowel actuele data als historische datasets. 
         Net als de OpenMeteo API, is de KNMI dataset makkelijk aan te passen. De user kan zelf kiezen welke variabelen beschikbaar zijn.
+
+        - **https://daggegevens.knmi.nl/klimatologie/daggegevens**
         '''
 )
 

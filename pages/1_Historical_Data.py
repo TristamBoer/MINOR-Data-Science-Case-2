f1605056aafa98a667c89a9e5c58f919b99b1a74
TristamBoer <127968977+TristamBoer@users.noreply.github.com>
2024-09-26 10:48:00 +0200

    Update 1_Historical_Data.py

--- pages/1_Historical_Data.py
+++ pages/1_Historical_Data.py
@@ -214,7 +214,7 @@ fig = px.scatter(data_frame=filtered_df,
 
 fig.update_layout(
     title=f'{variable3} gebaseerd op directie: {selected_wind_direction}',
-    yaxis_title='Max Wind Speed [km/h]',
+    yaxis_title=f'{variable3}',
     xaxis_title='Date',
     title_font_size=18,
 )

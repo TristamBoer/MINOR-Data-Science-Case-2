25bc0a16fe4880590e536a42dced7c97bf911e92
TristamBoer <127968977+TristamBoer@users.noreply.github.com>
2024-09-26 11:10:02 +0200

    Update 1_Historical_Data.py

--- pages/1_Historical_Data.py
+++ pages/1_Historical_Data.py
@@ -133,7 +133,7 @@ col1, col2, col3 = st.columns([1, 1, 4])
 
 with col1:
     st.header("Plotting Variable 1")
-    variable1 = st.checkbox("Selecteer eerste variabel:", [
+    variable1 = st.selectbox("Selecteer eerste variabel:", [
 	'date', 'temperature_2m_mean', 'apparent_temperature_mean',
 	'daylight_duration', 'sunshine_duration', 'precipitation_sum',
 	'rain_sum', 'snowfall_sum', 'precipitation_hours', 'wind_speed_10m_max',
@@ -149,7 +149,7 @@ with col1:
 
 with col2:
     st.header("Plotting Variable 2")
-    variable2 = st.checkbox("Selecteer tweede variabel:", [
+    variable2 = st.selectbox("Selecteer tweede variabel:", [
 	'date', 'temperature_2m_mean', 'apparent_temperature_mean',
 	'daylight_duration', 'sunshine_duration', 'precipitation_sum',
 	'rain_sum', 'snowfall_sum', 'precipitation_hours', 'wind_speed_10m_max',

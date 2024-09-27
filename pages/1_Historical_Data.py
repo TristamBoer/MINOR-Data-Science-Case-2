41c4589e89d91acab41730a1e9e86834151323c9
TristamBoer <127968977+TristamBoer@users.noreply.github.com>
2024-09-27 10:24:31 +0200

    Update 1_Historical_Data.py

--- pages/1_Historical_Data.py
+++ pages/1_Historical_Data.py
@@ -149,10 +149,10 @@ with col1:
 			     
 with col2:
     variable2 = st.selectbox("Selecteer tweede variabel:", [
-	'date', 'temperature_2m_mean', 'apparent_temperature_mean',
+	'temperature_2m_mean', 'apparent_temperature_mean',
 	'daylight_duration', 'sunshine_duration', 'precipitation_sum',
 	'rain_sum', 'snowfall_sum', 'precipitation_hours', 'wind_speed_10m_max',
-	'wind_gusts_10m_max', 'wind_direction_10m_dominant'
+	'wind_gusts_10m_max', 'wind_direction_10m_dominant', 'date'
 	])
 
 
